7b136a32ea38d05312c7c5740fda2f327622e1bd
UmbertoV <umberto.ottone@gmail.com>
2019-11-20 19:50:59 +0100

    Da perfezionare la media

---
+++ JSnack 9/main.js
@@ -0,0 +1,13 @@
+ // Calcola la somma e la media dei primi 10 numeri.
+
+// var numero = [] ;
+var media = 0;
+var somma = 0;
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+    somma = somma + i;
+    media = somma % 10;
+}
+
+console.log("La somma è " + somma);
+console.log("La media è " + media);
